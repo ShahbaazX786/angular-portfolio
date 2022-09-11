6238d2ec7e94724f9e87765ef15e1bd58cd79ff5
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-11 12:26:35 +0530

    CL:1.03- Added 2 new components

--- p2022/src/app/app.module.ts
+++ p2022/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
