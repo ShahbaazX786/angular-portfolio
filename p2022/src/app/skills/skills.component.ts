13c86794265b2c795dc784bd7792eea927275ef0
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-17 16:46:45 +0530

    CL:1.30- Added the svg icons by editing them. there seems to be some problem in console while loading svg's so gotta address that sheeet later.

--- p2022/src/app/skills/skills.component.ts
+++ p2022/src/app/skills/skills.component.ts
@@ -3,13 +3,85 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  skills = [
+    {
+      name:'HTML',
+      src:"../../assets/img/external/svg/html.svg",
+    },
+    {
+      name:'CSS',
+      src:'../../assets/img/external/svg/css..svg',
+    },
+    {
+      name:'Javascript',
+      src:'../../assets/img/external/svg/javascript.svg',
+    },
+    {
+      name:'MongoDB',
+      src:'../../assets/img/external/svg/mongodb.svg',
+    },
+    {
+      name:'API',
+      src:'../../assets/img/external/svg/api.svg',
+    },
+    {
+      name:'Bootstrap',
+      src:'../../assets/img/external/svg/bootstrap.svg',
+    },
+    {
+      name:'Bulma css',
+      src:'../../assets/img/external/svg/bulma..svg',
+    },
+    {
+      name:'Express JS',
+      src:'../../assets/img/external/svg/express-js.svg',
+    },
+    {
+      name:'Figma',
+      src:'../../assets/img/external/svg/figma..svg',
+    },
+    {
+      name:'Firebase',
+      src:'../../assets/img/external/svg/firebase.svg',
+    },
+    {
+      name:'Git',
+      src:'../../assets/img/external/svg/git..svg',
+    },
+    {
+      name:'Material UI',
+      src:'../../assets/img/external/svg/mui..svg',
+    },
+    {
+      name:'Node JS',
+      src:'../../assets/img/external/svg/nodejs.svg',
+    },
+    {
+      name:'Photoshop',
+      src:'../../assets/img/external/svg/photoshop.svg',
+    },
+    {
+      name:'SQL',
+      src:'../../assets/img/external/svg/sql.svg',
+    },
+    {
+      name:'Angular',
+      src:'../../assets/img/external/svg/angular.svg',
+    },
+    {
+      name:'Tailwind CSS',
+      src:'../../assets/img/external/svg/tailwind..svg',
+    },
+    {
+      name:'Search Engine Optimization',
+      src:'../../assets/img/external/svg/seo.svg',
+    },
+  ];
 }
