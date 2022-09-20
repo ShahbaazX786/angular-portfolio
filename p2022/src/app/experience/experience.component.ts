ac8aa4bc6f7726e795057e12e91b44e0d22c7f75
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-20 14:11:08 +0530

    CL:1.32- Added contact section.

--- p2022/src/app/experience/experience.component.ts
+++ p2022/src/app/experience/experience.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
-  styleUrls: ['./experience.component.css']
+  styleUrls: ['./experience.component.css'],
 })
 export class ExperienceComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  contacts = [
+    {
+      class: 'fa-brands fa-twitter',
+      social: 'Twitter',
+      username: 'ShahbaazX',
+    },
+    {
+      class: 'fa-solid fa-envelope',
+      social: 'Contact Me',
+      username: 'Drop me a Mail',
+    },
+    {
+      class: 'fa-brands fa-github',
+      social: 'Github',
+      username: 'ShahbaazX786',
+    },
+    {
+      class: 'fa-brands fa-linkedin',
+      social: 'LinkedIn',
+      username: 'Shaik Shahbaaz Alam',
+    },
+  ];
 }
