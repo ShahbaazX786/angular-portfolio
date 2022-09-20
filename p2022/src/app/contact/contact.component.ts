03aa10b104fbca06cd68f5924e7255b2418c0f1e
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-20 15:50:09 +0530

    CL:1.34- Mistaked contact component with experiencee component.

--- p2022/src/app/contact/contact.component.ts
+++ p2022/src/app/contact/contact.component.ts
@@ -3,13 +3,33 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  contacts = [
+    {
+      class: 'fa-brands fa-twitter',
+      social: 'Twitter',
+      username: 'ShahbaazX',
+    },
+    {
+      class: 'fa-solid fa-envelope',
+      social: 'Contact Me',
+      username: 'Drop me a Mail',
+    },
+    {
+      class: 'fa-brands fa-github',
+      social: 'Github',
+      username: 'ShahbaazX786',
+    },
+    {
+      class: 'fa-brands fa-linkedin',
+      social: 'LinkedIn',
+      username: 'Shaik Shahbaaz Alam',
+    },
+  ];
 }
--- p2022/src/app/experience/experience.component.ts
+++ p2022/src/app/experience/experience.component.ts
@@ -10,26 +10,4 @@ export class ExperienceComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  contacts = [
-    {
-      class: 'fa-brands fa-twitter',
-      social: 'Twitter',
-      username: 'ShahbaazX',
-    },
-    {
-      class: 'fa-solid fa-envelope',
-      social: 'Contact Me',
-      username: 'Drop me a Mail',
-    },
-    {
-      class: 'fa-brands fa-github',
-      social: 'Github',
-      username: 'ShahbaazX786',
-    },
-    {
-      class: 'fa-brands fa-linkedin',
-      social: 'LinkedIn',
-      username: 'Shaik Shahbaaz Alam',
-    },
-  ];
 }
