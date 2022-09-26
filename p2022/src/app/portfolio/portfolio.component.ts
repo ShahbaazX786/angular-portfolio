92e6ffa50b593a4d70671118146739417c9f3a03
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-09-26 13:22:23 +0530

    CL:1.42- Added portfolio section but needs to fix responsiveness and few bugs.

--- p2022/src/app/portfolio/portfolio.component.ts
+++ p2022/src/app/portfolio/portfolio.component.ts
@@ -12,4 +12,39 @@ export class PortfolioComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  portfolio_array = [
+    {
+      name:'Tindoggo',
+      img:'../../assets/img/external/Project-screenshots/Tindoggo.png'
+    },
+    {
+      name:'She-Hulk',
+      img:'../../assets/img/external/Project-screenshots/construction.png'
+    },
+    {
+      name:'Blog-EJS',
+      img:'../../assets/img/external/Project-screenshots/Blog.png'
+    },
+    {
+      name:'Todolist-EJS',
+      img:'../../assets/img/external/Project-screenshots/TodoList.png'
+    },
+    {
+      name:'Newsletter-API',
+      img:'../../assets/img/external/Project-screenshots/Newsletter.png'
+    },
+    {
+      name:'Drumkit',
+      img:'../../assets/img/external/Project-screenshots/drumkit.png'
+    },
+    {
+      name:'Simons-game',
+      img:'../../assets/img/external/Project-screenshots/simongame.png'
+    },
+    {
+      name:'slide-animation',
+      img:'../../assets/img/external/Project-screenshots/slideanimation.png'
+    },
+  ]
+
 }
