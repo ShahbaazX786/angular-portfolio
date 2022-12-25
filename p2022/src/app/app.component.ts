adba5924cb9f9c79c50e4c71fb7c2c563dca46d7
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-12-25 17:02:54 +0530

    CL:2.3.5

--- p2022/src/app/app.component.ts
+++ p2022/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+// import { Title } from '@angular/platform-browser';
 
 
 @Component({
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
 export class AppComponent {
   title = 'p2022';
 
-  constructor(private _titleservice:Title){
-    this._titleservice.setTitle($localize`${this.title}`);
-  }
+  // constructor(private _titleservice:Title){
+  //   this._titleservice.setTitle($localize`${this.title}`);
+  // }
 }
